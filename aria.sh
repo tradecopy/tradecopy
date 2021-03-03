fddad75fd20c4f9a20ebef8aa3ed60f7bff6903e
tradecopy <79205337+tradecopy@users.noreply.github.com>
2021-03-03 07:55:42 -0800

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -2,7 +2,7 @@ export MAX_DOWNLOAD_SPEED=0
 export MAX_CONCURRENT_DOWNLOADS=10
 trackers_list=$(curl -Ns https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection@master/all.txt | awk '$1' | tr '\n' ',')
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
-   --max-connection-per-server=10 --daemon=true \
+   --max-connection-per-server=15 --daemon=true \
    --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --bt-tracker="[$trackers_list]" --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
    --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M \
